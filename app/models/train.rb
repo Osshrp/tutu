4e4508c12ac34098bccfcdbd45fc23873c22783d
Osshrp <osshrp@gmail.com>
2017-04-01 18:40:54 +0300

    show seats quantity

--- app/models/train.rb
+++ app/models/train.rb
@@ -5,4 +5,31 @@ class Train < ApplicationRecord
   has_many :carriages
 
   validates :number, presence: true
+
+  # def inittalize
+  #   # @places = {}
+  # end
+
+  def second_class_cars
+    carriages.where("car_type = 'купе'").count
+  end
+
+  def third_class_cars
+    carriages.where("car_type = 'плацкарт'").count
+  end
+
+  def places
+    places = {}
+    carriages.each do |carriage|
+      if carriage.car_type == 'купе'
+        # byebug
+        places[:second_class_upper_places] = carriage.upper_places
+        places[:second_class_bottom_places] = carriage.bottom_places
+      else
+        places[:third_class_upper_places] = carriage.upper_places
+        places[:third_class_bottom_places] = carriage.bottom_places
+      end
+    end
+    places
+  end
 end
