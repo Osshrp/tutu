b0eb3e917c32358ac3562476f7e995f44cfe6560
Osshrp <osshrp@gmail.com>
2017-04-03 13:21:42 +0300

    station position and route validation

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,4 +4,9 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
 
   validates :title, presence: true
+
+  def update_postion(position, route)
+    station_in_route = RailwayStationsRoute.where(railway_station: self, route: route).first
+    station_in_route.save
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,69 +10,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_401_104_259) do
+ActiveRecord::Schema.define(version: 20170403072809) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'carriages', force: :cascade do |t|
-    t.string   'car_type'
-    t.integer  'number'
-    t.integer  'upper_places'
-    t.integer  'bottom_places'
-    t.datetime 'created_at',    null: false
-    t.datetime 'updated_at',    null: false
-    t.integer  'train_id'
-    t.index ['train_id'], name: 'index_carriages_on_train_id', using: :btree
+  create_table "carriages", force: :cascade do |t|
+    t.string   "car_type"
+    t.integer  "number"
+    t.integer  "upper_places"
+    t.integer  "bottom_places"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "train_id"
+    t.index ["train_id"], name: "index_carriages_on_train_id", using: :btree
   end
 
-  create_table 'railway_stations', force: :cascade do |t|
-    t.string   'title'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "railway_stations", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'railway_stations_routes', force: :cascade do |t|
-    t.integer 'railway_station_id'
-    t.integer 'route_id'
+  create_table "railway_stations_routes", force: :cascade do |t|
+    t.integer "railway_station_id"
+    t.integer "route_id"
+    t.integer "station_index"
   end
 
-  create_table 'routes', force: :cascade do |t|
-    t.string 'name'
+  create_table "routes", force: :cascade do |t|
+    t.string "name"
   end
 
-  create_table 'tickets', force: :cascade do |t|
-    t.string   'number'
-    t.datetime 'created_at',       null: false
-    t.datetime 'updated_at',       null: false
-    t.integer  'user_id'
-    t.integer  'train_id'
-    t.integer  'start_station_id'
-    t.integer  'end_station_id'
-    t.index ['end_station_id'], name: 'index_tickets_on_end_station_id', using: :btree
-    t.index ['start_station_id'], name: 'index_tickets_on_start_station_id', using: :btree
-    t.index ['train_id'], name: 'index_tickets_on_train_id', using: :btree
-    t.index ['user_id'], name: 'index_tickets_on_user_id', using: :btree
+  create_table "tickets", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "user_id"
+    t.integer  "train_id"
+    t.integer  "start_station_id"
+    t.integer  "end_station_id"
+    t.index ["end_station_id"], name: "index_tickets_on_end_station_id", using: :btree
+    t.index ["start_station_id"], name: "index_tickets_on_start_station_id", using: :btree
+    t.index ["train_id"], name: "index_tickets_on_train_id", using: :btree
+    t.index ["user_id"], name: "index_tickets_on_user_id", using: :btree
   end
 
-  create_table 'trains', force: :cascade do |t|
-    t.string   'number'
-    t.datetime 'created_at',         null: false
-    t.datetime 'updated_at',         null: false
-    t.integer  'current_station_id'
-    t.integer  'route_id'
-    t.index ['current_station_id'], name: 'index_trains_on_current_station_id', using: :btree
-    t.index ['route_id'], name: 'index_trains_on_route_id', using: :btree
+  create_table "trains", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "current_station_id"
+    t.integer  "route_id"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id", using: :btree
+    t.index ["route_id"], name: "index_trains_on_route_id", using: :btree
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'carriages', 'trains'
-  add_foreign_key 'tickets', 'trains'
-  add_foreign_key 'tickets', 'users'
-  add_foreign_key 'trains', 'railway_stations', column: 'current_station_id'
-  add_foreign_key 'trains', 'routes'
+  add_foreign_key "carriages", "trains"
+  add_foreign_key "tickets", "trains"
+  add_foreign_key "tickets", "users"
+  add_foreign_key "trains", "railway_stations", column: "current_station_id"
+  add_foreign_key "trains", "routes"
 end
