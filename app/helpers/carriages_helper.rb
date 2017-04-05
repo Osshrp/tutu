9768da11e2b2d65dff3ae0ba4b42378c6e306a05
Osshrp <osshrp@gmail.com>
2017-04-05 10:33:34 +0300

    show carriage info

---
+++ app/helpers/carriages_helper.rb
@@ -0,0 +1,10 @@
+module CarriagesHelper
+  def self.car_type_hash
+    {
+      'FirstClassCarriage' => 'first_class_carriage',
+      'SecondClassCarriage' => 'second_class_carriage',
+      'ThirdClassCarriage' => 'third_class_carriage',
+      'CouchCarriage' => 'couch_carriage'
+    }
+  end
+end
