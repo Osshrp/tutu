6670d4eddc700f9b0cf27e3e6e39f310c12c6a66
Osshrp <osshrp@gmail.com>
2017-04-09 22:22:50 +0300

    try to show search resalt

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,12 +4,13 @@ class SearchesController < ApplicationController
   end
 
   def create
-    @trains = Search.trains(search_params[:start_station], search_params[:last_station])
+    @trains = Search.trains(search_params[:start_station_id], search_params[:last_station_id])
+    render :show
   end
 
   private
 
   def search_params
-    params.require(:stations).permit(:start_station, :last_station)
+    params.require(:stations).permit(:start_station_id, :last_station_id)
   end
 end
