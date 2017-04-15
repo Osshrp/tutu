d9cbc3fc5bf772cdaeab0f8bce7967758e70ab7d
Osshrp <osshrp@gmail.com>
2017-04-15 12:41:45 +0300

    bootstraping user interface

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
+  before_action :set_railway_stations
+
   def show
-    @railway_stations = RailwayStation.all
   end
 
   def create
@@ -9,5 +10,11 @@ class SearchesController < ApplicationController
     @start_station = RailwayStation.find(params[:start_station_id])
     @end_station = RailwayStation.find(params[:end_station_id])
     render :show
+  end
+
+  private
+
+  def set_railway_stations
+    @railway_stations = RailwayStation.all
   end
 end
