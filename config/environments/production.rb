1cc9300c22a06ff459b4b599e656855e5df377b9
Osshrp <osshrp@gmail.com>
2017-04-16 17:55:13 +0300

    some fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,12 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.assets.compile = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: "intense-temple-27407.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
