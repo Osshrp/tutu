4985c3e1545c0f5d03976c658e577cd03c297fe9
Osshrp <osshrp@gmail.com>
2017-04-20 09:57:43 +0300

    some fix

--- app/assets/javascripts/railway_stations.js
+++ app/assets/javascripts/railway_stations.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
   $('a.edit_station').click(function(e) {
     e.preventDefault();
-    var station_id;
-    var form;
-    var title;
-
-    station_id = $(this).data('stationId');
-    form = $('#edit_railway_station_' + station_id);
-    title = $('#railway_station_title_' + station_id);
+    var station_id = $(this).data('stationId');
+    var form = $('#edit_railway_station_' + station_id);
+    var title = $('#railway_station_title_' + station_id);
 
     if (!$(this).hasClass('cancel')) {
       $(this).html('Отменить');
--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
   $('.search_form').submit(function() {
-    var start_station;
-    var end_station;
-
-    start_station = $(this).find('#start_station_id');
-    end_station = $(this).find('#end_station_id');
+    var start_station = $(this).find('#start_station_id');
+    var end_station = $(this).find('#end_station_id');
 
     if (start_station.val() == end_station.val()) {
       return false;
